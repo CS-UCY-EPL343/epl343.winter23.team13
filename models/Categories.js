7bda27e41be1d2ee0e8abe08468e3a616b7c41d9
Orestis Kalogirou <okalog01@ucy.ac.cy>
2023-11-16 01:21:50 +0200

    Added Category Instances

--- models/Categories.js
+++ models/Categories.js
@@ -10,3 +10,30 @@ const CategorySchema = new mongoose.Schema({
 
   const Category = mongoose.model("Category", CategorySchema)
   module.exports = Category
+
+
+  const categoriesData = [
+    { category_name: "Home" },
+    { category_name: "Tools" },
+    { category_name: "Vehicles" },
+    { category_name: "Electronics" },
+    { category_name: "Outdoors" },
+    { category_name: "Gaming" },
+    { category_name: "Photography" },
+  ];
+  
+  // async function saveCategories() {
+  //   try {
+  //     // Save each category asynchronously
+  //     const savedCategories = await Promise.all(
+  //       categoriesData.map(category => new Category(category).save())
+  //     );
+  
+  //     console.log("Categories saved successfully:", savedCategories);
+  //   } catch (error) {
+  //     console.error("Error saving categories:", error);
+  //   }
+  // }
+  
+  // // Call the function to save categories
+  // saveCategories();
